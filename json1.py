ba4d04b0b364f1d6f085e990c9126ee16313d0c4
thenicholascoder <thenicholashernandez@gmail.com>
2020-02-27 21:21:08 +0800

    reupload

--- json1.py
+++ json1.py
@@ -1,7 +1,7 @@
 #import the json library
 import json
 
-#use the triple quoted string
+#use the triple quoted string to put json in there
 data = '''
 {
   "name" : "Chuck",
@@ -14,7 +14,9 @@ data = '''
    }
 }'''
 
+#json will turn data into dictionary kvp with nested dictionary kvp
 #loads means loads means load from string
+#then  parse the data into a structured object or dom
 info = json.loads(data)
 
 #info["name"] = it means get the info value of the key name
--- json2.py
+++ json2.py
@@ -14,13 +14,14 @@ data = '''
   }
 ]'''
 
+#json will turn data into json list with nested dictionary kvp
 #json.loads() = load from strings
 info = json.loads(data)
 
 #len() = get the length of the strings inside info
 print('User count:', len(info))
 
-#for each item in info:
+#for each item in info, itterate through info:
 for item in info:
 
     #print Name and also the value of name
